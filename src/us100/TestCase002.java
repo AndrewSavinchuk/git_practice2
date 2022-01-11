f0f5572300d724ace40c342c876c11552dd2ed2f
AndrewSavinchuk <savawise@gmail.com>
2022-01-11 10:28:50 -0600

    Text2 added

--- src/us100/TestCase002.java
+++ src/us100/TestCase002.java
@@ -3,5 +3,6 @@ package us100;
 public class TestCase002 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Git class is cool");
     }
 }
